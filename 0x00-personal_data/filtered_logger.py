5b93d7f11e5b382fc9d1fa214c07bb930fe3a221
Gich-M <gichmuriuki21@gmail.com>
2024-11-08 16:17:17 +0300

    filtered_logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module filtered-logger."""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    pattern = f"({'|'.join(fields)})=[^{separator}]*"
+    return re.sub(
+        pattern,
+        lambda match: f"{
+            match.group(1)}={redaction}",
+        message)
