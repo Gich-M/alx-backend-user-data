1f66619cd9a04771d90f88868db7e92ce2ce46e1
Gich-M <gichmuriuki21@gmail.com>
2024-11-08 16:25:03 +0300

    filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,14 @@
 """Module filtered-logger."""
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """Returns the log message obfuscated."""
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(
