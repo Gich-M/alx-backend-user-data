2deeba1fb12b6f09f30eeeca4344a18ff270220b
Gich-M <gichmuriuki21@gmail.com>
2024-12-14 11:28:02 +0300

    BasicAuth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+"""Module BasicAuth"""
+
+from api.v1.auth import Auth
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth and implements basic authentication
+    """
+
