f6fd8e54d12474c2b9d3ed0c1a47863395787ac4
Gich-M <gichmuriuki21@gmail.com>
2024-12-14 18:30:06 +0300

    Updated SessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """Modules SessionAuth."""
+import uuid
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Session authentication class.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str=None) -> str:
+        """Creates a Session ID for a `user_id`."""
+        if isinstance(user_id, str):
+            session_id = uuid.uuid4()
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
