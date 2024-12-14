0777087a4cffbc2974c27711e637c4b7ea39eb8f
Gich-M <gichmuriuki21@gmail.com>
2024-12-14 18:31:02 +0300

    Updated SessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,7 +9,7 @@ class SessionAuth(Auth):
     """
     user_id_by_session_id = {}
 
-    def create_session(self, user_id: str=None) -> str:
+    def create_session(self, user_id: str = None) -> str:
         """Creates a Session ID for a `user_id`."""
         if isinstance(user_id, str):
             session_id = uuid.uuid4()
