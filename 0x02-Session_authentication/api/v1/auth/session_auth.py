3057788c756c0c286a697122e676dad2f423ff03
Gich-M <gichmuriuki21@gmail.com>
2024-12-14 18:40:56 +0300

    User Session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Modules SessionAuth."""
-import uuid
+from uuid import uuid4
 from .auth import Auth
 
 
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """Creates a Session ID for a `user_id`."""
         if isinstance(user_id, str):
-            session_id = uuid.uuid4()
+            session_id = str(uuid4)()
             self.user_id_by_session_id[session_id] = user_id
             return session_id
         return None
@@ -21,4 +21,4 @@ class SessionAuth(Auth):
         """Retrieves the `user_id` associated with a `session_id`."""
         if isinstance(session_id, str):
             return self.user_id_by_session_id.get(session_id)
-        return None
+
