e6d36c4f851f96e839d76f709a98c3d2cffa61cf
Gich-M <gichmuriuki21@gmail.com>
2024-12-14 12:17:27 +0300

    BasicAuth tasks

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-
-
 """Module BasicAuth"""
 
 import base64
 from typing import Optional, TypeVar
-from api.v1.auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
