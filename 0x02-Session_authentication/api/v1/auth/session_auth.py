cc909a18c9db99c7b87e1d2dcbdad276a5acc202
Gich-M <gichmuriuki21@gmail.com>
2024-12-14 18:46:06 +0300

    User Session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """Creates a Session ID for a `user_id`."""
         if isinstance(user_id, str):
-            session_id = str(uuid4)()
+            session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
         return None
